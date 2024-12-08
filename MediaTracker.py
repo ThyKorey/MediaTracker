c781e031cebe8dd73b30e6f7baa0c2eb9bebdc17
ThyKorey <koreybibie@gmail.com>
2024-12-08 13:10:52 -0500

    Significant MediaTracker.py Update
    laid out things differently, made the GUI for the entirety of the main software window and also finished the about window, next is to work on program functionality and to change some of the main software window layout

--- MediaTracker.py
+++ MediaTracker.py
@@ -3,9 +3,7 @@ from tkinter import filedialog as FileExplorer # Will be used for saving and loa
 from tkinter import ttk # Themed Tkinter
 from PIL import Image, ImageTk # used to import images
 
-def main():
-    App = MediaTracker()
-    App.mainloop() # Lets the program loop so it can open and run
+
 
 class MediaTracker(tk.Tk): # Main Window Part 1: Defining characteristics
     def __init__(self):
@@ -19,21 +17,7 @@ class MediaTracker(tk.Tk): # Main Window Part 1: Defining characteristics
         MainWindow = MainWindowFrame(self)
         MainWindow.pack(fill="both", expand= True) # Lets us render the window
 
-class TopSoftwareWindow: # Software Window area
-    def __init__(self):
-        self.SoftwareWindow = tk.Toplevel()
-        self.SoftwareWindow.geometry("800x600")
-        self.SoftwareWindow.resizable(False, False)
-        self.SoftwareWindow.title("Software Window")
-        self.SoftwareWindow.iconbitmap('MediaTracker_Icon.ico') 
 
-class TopAboutWindow: # About Software area
-    def __init__(self):
-        self.AboutWindow = tk.Toplevel()
-        self.AboutWindow.geometry("300x200")
-        self.AboutWindow.resizable(False, False)
-        self.AboutWindow.title("About Window")
-        self.AboutWindow.iconbitmap('MediaTracker_Icon.ico') 
 
 class MainWindowFrame(ttk.Frame): # Main Window Part 2: The interface
     def __init__(self, parent):
@@ -48,7 +32,7 @@ class MainWindowFrame(ttk.Frame): # Main Window Part 2: The interface
         self.About_btn = ttk.Button(self, text="About Software", command=self.CallAboutWindow)
         self.Quit_btn = ttk.Button(self, text="Quit Software", command=parent.destroy)
 
-        # packs them so they appear on the window
+        # Using .pack here as the main window just needs to be simple and requires no complex layout
 
         self.label.pack()
         self.New_btn.pack(fill= "x") # (fill= "x") stretches them to cover the entire window
@@ -59,12 +43,75 @@ class MainWindowFrame(ttk.Frame): # Main Window Part 2: The interface
 
     def CallTopSoftwareWindow(self): 
 
-        self.Looper = TopSoftwareWindow()
-        self.Looper.SoftwareWindow.mainloop()
+        self.CallOnWindow = TopSoftwareWindow()
 
     def CallAboutWindow(self):
-        self.Looper = TopAboutWindow()        
-        self.Looper.AboutWindow.mainloop()
+
+        self.CallOnWindow = TopAboutWindow()        
+
+
+
+class TopSoftwareWindow: # Software Window area
+    def __init__(self):
+        self = tk.Toplevel()
+        self.geometry("800x600")
+        self.resizable(False, False)
+        self.title("Software Window")
+        self.iconbitmap('MediaTracker_Icon.ico') 
+
+        # Everything that appears on this window
+        self.Entry_Field = ttk.Entry(self)
+
+        self.Text_List = tk.Listbox(self)
+        self.Category_List = tk.Listbox(self)
+
+        self.New_Entry_Btn = ttk.Button(self, text="New Entry")
+        self.Save_File_Btn = ttk.Button(self, text="Save File")
+        self.Load_File_Btn = ttk.Button(self, text="Load File")
+        self.Quit_Btn = ttk.Button(self, text="Quit")
+
+        # Note to self, Replace .pack with .grid more complex layout also tryout .place since windows cant be resized
+        self.Entry_Field.place(x= 0, y= 0, width= 724)
+
+        self.New_Entry_Btn.place(x= 725, y= 0)
+
+        self.Text_List.place(x= 0, y= 25, width= 625, height= 570)
+        self.Category_List.place(x= 626, y= 25, width= 99, height= 570)
+
+        self.Save_File_Btn.place(x= 725, y= 25)
+        self.Load_File_Btn.place(x= 725, y= 50)
+        self.Quit_Btn.place(x= 725, y= 570)
+
+
+
+class TopAboutWindow: # About Software area
+    def __init__(self):
+        self = tk.Toplevel()
+        self.geometry("300x200")
+        self.resizable(False, False)
+        self.title("About Window")
+        self.iconbitmap('MediaTracker_Icon.ico')
+
+        self.image = Image.open("MediaTracker_Image.jpg") # Opens the MediaTracker logo Image
+        self.photo = ImageTk.PhotoImage(self.image) # Converts the image into something tkinter can work with
+
+        self.img_label = tk.Label(self, image=self.photo) # puts the image on a empty label
+        self.credit = ttk.Label(self, text="Korey Bibie 2024")
+        self.label1 = ttk.Label(self, text="This program was designed to help those who")
+        self.label2 = ttk.Label(self, text="struggle like I do keeping track of Media")
+
+        self.img_label.pack(side= "top")
+        
+        self.label1.pack(side="top")
+        self.label2.pack(side="top")
+
+        self.credit.pack(side="top")
+
+def main():
+    App = MediaTracker()
+    App.mainloop() # Lets the program loop so it can open and run
+
+main()
 
 main()
 
